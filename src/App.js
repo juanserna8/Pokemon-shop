86eb68aa4c4fd83ddda8bfe682d0db767a8b8ee1
unknown <you@example.com>
2022-03-29 08:16:42 +1100

    feat: tailwind's boilerplate

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <p className='bg-white'>Hello Kike</p>
         </a>
       </header>
     </div>
